5f0f061bb08100e254a854652e6e396d600423a3
Adam Smith <coloradoasmith@gmail.com>
2020-12-10 16:16:18 -0700

    notifications working 5.4 complete

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
+import NotificationER from "./components/NotificationER";
+import NotificationOK from "./components/NotificationOK";
+import "./App.css";
 
-const Notification = ({ message }) => {
-  if (message === null) {
-    return null;
-  }
+// const Notification = ({ message }) => {
+//   if (message === null) {
+//     return null;
+//   }
 
-  return <div className="error">{message}</div>;
-};
+//   return <div className="error">{message}</div>;
+// };
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
@@ -18,6 +21,7 @@ const App = () => {
   const [newBlogAuthor, setNewBlogAuthor] = useState("");
   const [newBlogUrl, setNewBlogUrl] = useState("");
   const [errorMessage, setErrorMessage] = useState(null);
+  const [comfirmMessage, setComfirmMessage] = useState(null);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
@@ -91,18 +95,34 @@ const App = () => {
 
   const addBlog = (event) => {
     event.preventDefault();
+
     const blogObject = {
       title: newBlogTitle,
       author: newBlogAuthor,
       url: newBlogUrl,
     };
 
-    blogService.create(blogObject).then((returnedBlog) => {
-      setBlogs(blogs.concat(returnedBlog));
-      setNewBlogTitle("");
-      setNewBlogUrl("");
-      setNewBlogAuthor("");
-    });
+    blogService
+      .create(blogObject)
+      .then((returnedBlog) => {
+        setBlogs(blogs.concat(returnedBlog));
+        setNewBlogTitle("");
+        setNewBlogUrl("");
+        setNewBlogAuthor("");
+
+        setComfirmMessage(
+          `a new blog ${blogObject.title} by ${blogObject.author} was added : )`
+        );
+        setTimeout(() => {
+          setComfirmMessage(null);
+        }, 5000);
+      })
+      .catch((error) => {
+        setErrorMessage(error.response.data.error);
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+      });
   };
 
   const handleBlogAuthorChange = (e) => {
@@ -119,18 +139,12 @@ const App = () => {
   const blogForm = () => (
     <form onSubmit={addBlog}>
       <h2>New Blog</h2>
-      <p>
-        title:
-        <input value={newBlogTitle} onChange={handleBlogTitleChange} />
-      </p>
-      <p>
-        author:
-        <input value={newBlogAuthor} onChange={handleBlogAuthorChange} />
-      </p>
-      <p>
-        url:
-        <input value={newBlogUrl} onChange={handleBlogUrlChange} />
-      </p>
+      title:
+      <input value={newBlogTitle} onChange={handleBlogTitleChange} />
+      author:
+      <input value={newBlogAuthor} onChange={handleBlogAuthorChange} />
+      url:
+      <input value={newBlogUrl} onChange={handleBlogUrlChange} />
       <button type="submit">create</button>
     </form>
   );
@@ -139,16 +153,20 @@ const App = () => {
     <div>
       <h2>blogs</h2>
 
-      <Notification message={errorMessage} />
+      <br></br>
+      {errorMessage !== null ? (
+        <NotificationER message={errorMessage} />
+      ) : (
+        <NotificationOK message={comfirmMessage} />
+      )}
+      <br></br>
 
       {user === null ? (
         loginForm()
       ) : (
         <div>
-          <p>
-            {user.name} logged-in <button onClick={handleLogout}>logout</button>
-            {blogForm()}
-          </p>
+          {user.name} logged-in <button onClick={handleLogout}>logout</button>
+          {blogForm()}
           <ul>
             {blogs.map((blog) => (
               <Blog key={blog.id} blog={blog} />
