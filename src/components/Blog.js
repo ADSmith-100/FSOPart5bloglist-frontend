b467895e47566863d12618f1825115f197d9dc0c
Adam Smith <coloradoasmith@gmail.com>
2020-12-12 13:57:06 -0700

    hide and display extra info working 5.7 complete

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,46 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from "react";
 
-export default Blog
+const Blog = ({ blog, username }) => {
+  const [extraDataVisible, setExtraDataVisible] = useState(false);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const handleView = (e) => {
+    e.preventDefault();
+    setExtraDataVisible(true);
+  };
+
+  const handleHideData = (e) => {
+    e.preventDefault();
+    setExtraDataVisible(false);
+  };
+
+  return (
+    <div>
+      {extraDataVisible === false ? (
+        <div style={blogStyle}>
+          {blog.title} {blog.author} <button onClick={handleView}>view</button>
+        </div>
+      ) : (
+        <div style={blogStyle}>
+          <p>
+            {blog.title} <button onClick={handleHideData}>hide</button>
+          </p>
+          <p>{blog.author}</p>
+          <p>{blog.url}</p>
+          <p>
+            likes: {blog.likes} <button>like</button>{" "}
+          </p>
+          <p>user: {blog.user.username}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
